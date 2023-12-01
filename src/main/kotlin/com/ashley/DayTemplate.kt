a7f6fb4d5c3eaadf8dfbc5e945c39d7a25813080
Ashley Venn <ashleyevavenn@gmail.com>
2023-11-30 17:47:05 -0800

    Initial check-in

---
+++ src/main/kotlin/com/ashley/DayTemplate.kt
@@ -0,0 +1,30 @@
+package com.ashley
+
+import kotlin.math.log
+
+class DayTemplate {
+
+    private fun readData(): Nothing? {
+        val readData: Nothing? = null;
+        val resource = ClassLoader.getSystemResource("test.input")
+        resource.readText().lines().forEach { line ->
+            if (line != "") {
+                println("We read things, yay!")
+            }
+        }
+
+        return readData;
+    }
+
+    fun answer1(): Nothing? {
+        val data = readData();
+
+        return data;
+    }
+
+    fun answer2(): Nothing? {
+        val data = readData();
+
+        return data;
+    }
+}
---
+++ src/main/kotlin/com/ashley/main.kt
@@ -0,0 +1,10 @@
+package com.ashley
+
+fun main(args: Array<String>) {
+    val dayAnswers = DayTemplate()
+    println("Hello world")
+
+    println("Answer 1 " + dayAnswers.answer1())
+
+    println("Answer 2 " + dayAnswers.answer2())
+}
